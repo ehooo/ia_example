81b9cb2fba3e3ad9bfb7ed630d0d35e671bb2d06
ehooo <web.ehooo@gmail.com>
2021-10-23 20:05:51 +0200

    Manage error connections

--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@ app = FastAPI(
 )
 logger = logging.getLogger(__name__)
 
-TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
-    host=os.environ.get('TENSORFLOW_HOST', 'localhost'),
-    post=os.environ.get('TENSORFLOW_API_REST_PORT', 8501),
-    model=os.environ.get('MODEL_NAME', 'resnet'),
+TENSORFLOW_URL = "http://{host}:{post}/v1/models/{model}:predict".format(
+    host=os.environ.get("TENSORFLOW_HOST", "localhost"),
+    post=os.environ.get("TENSORFLOW_API_REST_PORT", 8501),
+    model=os.environ.get("MODEL_NAME", "resnet"),
 )
 
 
@@ -27,8 +27,8 @@ TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
                         "title": "Response",
                         "type": "object",
                         "properties": {
-                            'class': {
-                                'title': 'TensorFlow response', 'type': 'int',
+                            "class": {
+                                "title": "TensorFlow response", "type": "int",
                             }
                         },
                     },
@@ -51,8 +51,8 @@ TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
                         "title": "Response",
                         "type": "object",
                         "properties": {
-                            'detail': {
-                                'title': 'Error message', 'type': 'string',
+                            "detail": {
+                                "title": "Error message", "type": "string",
                             }
                         },
                     },
@@ -75,8 +75,8 @@ TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
                         "title": "Tensorflow error response",
                         "type": "object",
                         "properties": {
-                            'detail': {
-                                'title': 'Error message', 'type': 'string',
+                            "detail": {
+                                "title": "Error message", "type": "string",
                             }
                         },
                     },
@@ -85,6 +85,12 @@ TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
                             "summary": "Invalid response from TensorFlow",
                             "value": {
                                 "detail": "Error processing file",
+                            },
+                        },
+                        "connection error": {
+                            "summary": "Cannot connect to TensorFlow",
+                            "value": {
+                                "detail": "Cannot connect to TensorFlow",
                             },
                         }
                     },
@@ -95,26 +101,32 @@ TENSORFLOW_URL = 'http://{host}:{post}/v1/models/{model}:predict'.format(
     },
 )
 async def resnet(img: UploadFile = File(...)):
-    if not img.content_type.startswith('image/'):
+    if not img.content_type.startswith("image/"):
         logger.debug("Invalid file", extra={
-            'Content-Type': img.content_type,
-            'Filename': img.filename
+            "Content-Type": img.content_type,
+            "Filename": img.filename
         })
         raise HTTPException(status_code=400, detail="Invalid content type")
 
     logger.debug("Read file", extra={
-        'Content-Type': img.content_type,
-        'Filename': img.filename
+        "Content-Type": img.content_type,
+        "Filename": img.filename
     })
     contents = await img.read()
-    jpeg_bytes = base64.b64encode(contents).decode('utf-8')
+    jpeg_bytes = base64.b64encode(contents).decode("utf-8")
 
     predict_request = '{"instances" : [{"b64": "%s"}]}' % jpeg_bytes
-    response = requests.post(TENSORFLOW_URL, data=predict_request)
+    try:
+        response = requests.post(TENSORFLOW_URL, data=predict_request)
+    except requests.ConnectionError:
+        logger.exception("Cannot connect to TensorFlow", extra={
+            "url": TENSORFLOW_URL,
+        })
+        raise HTTPException(status_code=500, detail="Cannot connect to TensorFlow")
     logger.info("Response from TensorFlow", extra={
-        'raw': response.text,
-        'time': response.elapsed.total_seconds(),
-        'status': response.status_code,
+        "raw": response.text,
+        "time": response.elapsed.total_seconds(),
+        "status": response.status_code,
     })
     tf_data = response.json()
     if response.status_code != 200:
@@ -128,10 +140,10 @@ async def resnet(img: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail="Unexpected response from TensorFlow")
 
     return {
-        'class': classes
+        "class": classes
     }
 
 if __name__ == "__main__":
     import uvicorn
-    reload = '--reload' in os.environ.get('API_ARGS', '')
+    reload = "--reload" in os.environ.get("API_ARGS", "")
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
